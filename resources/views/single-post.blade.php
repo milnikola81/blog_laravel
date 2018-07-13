fbf868384200bf89ddb25b8fb3ea63f611c58559
jelena <jelena>
2018-07-13 19:41:58 +0200

    initial commit try 2

--- resources/views/single-post.blade.php
+++ resources/views/single-post.blade.php
@@ -1,7 +1,11 @@
 @extends ('layouts.master')
 
 @section('content')
-    <div>
-        {{ $post->title }}
-    </div>
+    <div class="blog-post">
+        <h2 class="blog-post-title">{{ $post->title }}</h2>
+        <p class="blog-post-meta">{{ $post->created_at }}</p>
+
+        <p>{{ $post->body }}</p>
+
+    </div><!-- /.blog-post -->
 @endsection
